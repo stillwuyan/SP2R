c1ea78cecf91138a4d747a5582d9cd09768d5a9d
xuqing <xuqing@neusoft.com>
2016-03-24 07:38:52 +0000

    fix(plugin): add gst-video libs for linker

--- src/plugins/sdlvideosink/target.mk
+++ src/plugins/sdlvideosink/target.mk
@@ -1,4 +1,5 @@
 PROJECT := sdlvideosink
 SRC     := $(wildcard $(PROJECT)/*.c)
+EXTLIBS := $(shell pkg-config --libs gstreamer-video-1.0)
 
-$(eval $(call TARGET_plugin, $(PROJECT)))
+$(eval $(call TARGET_plugin, $(PROJECT), $(EXTLIBS)))
